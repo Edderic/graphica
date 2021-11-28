bde84ff6fbf81cca0acb54739bcc943afd6f2ef5
Edderic Ugaddan <eugaddan@panoramaed.com>
2021-11-28 10:59:43 -0500

    create setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+"""
+Setup
+"""
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="linx",
+    version="0.0.1",
+    author="Edderic Ugaddan",
+    author_email="edderic@gmail.com",
+    description="Tools for Probabilistic Graphical Modeling",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/edderic/linx",
+    project_urls={
+        "Bug Tracker": "https://github.com/edderic/linx/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    ],
+    packages=["linx"],
+    python_requires=">=3.6",
+)
