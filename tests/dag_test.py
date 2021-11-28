061b59f49b5fdabc9129f57f1c89963419272895
Edderic Ugaddan <eugaddan@panoramaed.com>
2021-11-28 15:22:48 -0500

    add tests for get_neighbors, get_parents

---
+++ tests/dag_test.py
@@ -0,0 +1,21 @@
+from ..linx.ds import DirectedAcyclicGraph as DAG
+
+
+def test_get_neighbors():
+    dag = DAG()
+    dag.add_edge("A", "B")
+    dag.add_edge("B", "C")
+
+    assert ["B"] == dag.get_neighbors("A")
+    assert ["A", "C"] == dag.get_neighbors("B")
+    assert ["B"] == dag.get_neighbors("C")
+
+
+def test_get_parents():
+    dag = DAG()
+    dag.add_edge("A", "B")
+    dag.add_edge("B", "C")
+
+    assert [] == dag.get_parents("A")
+    assert ["A"] == dag.get_parents("B")
+    assert ["B"] == dag.get_parents("C")
