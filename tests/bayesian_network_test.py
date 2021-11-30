86f0bd9feae95bda2598401db65818e2f4f80710
Edderic Ugaddan <eugaddan@panoramaed.com>
2021-11-30 08:11:42 -0500

    implement BayesianNetwork#to_markov_network

--- tests/bayesian_network_test.py
+++ tests/bayesian_network_test.py
@@ -50,3 +50,52 @@ def test_find_cpt_for_node():
     assert bayesian_network.find_cpt_for_node('X') == cpt_3
     assert bayesian_network.find_cpt_for_node('A') == cpt_2
     assert bayesian_network.find_cpt_for_node('Y') == cpt_1
+
+
+def test_to_markov_network():
+    bayesian_network = BN()
+    df = pd.DataFrame([
+        {'X': 0, 'Y': 0, 'count': 0.25},
+        {'X': 0, 'Y': 1, 'count': 0.75},
+        {'X': 1, 'Y': 0, 'count': 0.6},
+        {'X': 1, 'Y': 1, 'count': 0.4},
+    ])
+
+    cpt_1 = CPT(
+        df=df,
+        outcomes=['Y'],
+        givens=['X']
+    )
+
+    bayesian_network.add_edge(cpt_1)
+
+    df_2 = pd.DataFrame([
+        {'X': 0, 'A': 0, 'count': 0.4},
+        {'X': 0, 'A': 1, 'count': 0.6},
+        {'X': 1, 'A': 0, 'count': 0.7},
+        {'X': 1, 'A': 1, 'count': 0.3},
+    ])
+
+    cpt_2 = CPT(
+        df=df_2,
+        outcomes=['A'],
+        givens=['X']
+    )
+
+    bayesian_network.add_edge(cpt_2)
+
+    df_3 = pd.DataFrame([
+        {'X': 0, 'count': 0.2},
+        {'X': 1, 'count': 0.8}
+    ])
+
+    cpt_3 = CPT(
+        df=df_3,
+        outcomes=['X'],
+    )
+
+    bayesian_network.add_node(cpt_3)
+
+    markov_network = bayesian_network.to_markov_network()
+    factors = markov_network.get_factors()
+    assert len(factors) == 3
