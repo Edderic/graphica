4134b30f2109e7ae64baf800558d3f18551c9b47
Edderic Ugaddan <eugaddan@panoramaed.com>
2021-11-30 17:22:22 -0500

    test Factors#remove(factor)

--- tests/markov_network_test.py
+++ tests/markov_network_test.py
@@ -120,3 +120,45 @@ def test_get_variables():
     markov_network.add_factor(factor_3)
 
     assert set(markov_network.get_variables()) == {'X', 'Y', 'A'}
+
+
+def test_remove_factors():
+    markov_network = MarkovNetwork()
+
+    df = pd.DataFrame([
+        {'X': 0, 'Y': 0, 'count': 0.25},
+        {'X': 0, 'Y': 1, 'count': 0.75},
+        {'X': 1, 'Y': 0, 'count': 0.6},
+        {'X': 1, 'Y': 1, 'count': 0.4},
+    ])
+
+    cpt_1 = CPT(
+        df=df,
+        outcomes=['Y'],
+        givens=['X']
+    )
+
+    factor_1 = Factor(cpt=cpt_1)
+    markov_network.add_factor(factor_1)
+
+    df_2 = pd.DataFrame([
+        {'X': 0, 'A': 0, 'count': 0.4},
+        {'X': 0, 'A': 1, 'count': 0.6},
+        {'X': 1, 'A': 0, 'count': 0.7},
+        {'X': 1, 'A': 1, 'count': 0.3},
+    ])
+
+    cpt_2 = CPT(
+        df=df_2,
+        outcomes=['A'],
+        givens=['X']
+    )
+
+    factor_2 = Factor(cpt=cpt_2)
+    markov_network.add_factor(factor_2)
+
+    markov_network.remove_factor(factor_1)
+
+    factors = markov_network.get_factors()
+    assert len(factors) == 1
+    assert factor_2 in factors
