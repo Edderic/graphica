544b62d8bca0bddea41209293c92a017fa2a9581
Edderic Ugaddan <eugaddan@panoramaed.com>
2021-12-15 21:00:48 -0500

    log_factor tests pass

--- linx/log_factor.py
+++ linx/log_factor.py
@@ -39,9 +39,50 @@ class LogFactor:
                 + "entries with variables."
             )
 
+        if any(self.df['value'] == -np.inf):
+            raise ArgumentError(
+                "Must not have negative infinity values. df:\n"
+                + f"{self.df}"
+            )
+
+        if self.df.shape[0] == 0:
+            raise ArgumentError(
+                    f"Dataframe {self.df} is empty."
+                    )
+
     def __repr__(self):
         return f"\nLogFactor(\nvariables: {self.get_variables()}" \
-            + f", \ndf: \n{self.df})\n"
+            + f", \ndf: \n{self.df}\n)\n"
+
+    def filter(self, query):
+        """
+        Apply filters of a query to this factor.
+
+        Parameters:
+            query: Query
+                Implements get_filters. Each item is a dict.
+                key: string
+                    Name of the variable.
+                value: callable or string or float or integer
+                    We use this for filtering.
+
+        Returns: LogFactor
+        """
+        df = self.df
+        filters = query.get_filters()
+
+        for f in filters:
+            key = list(f.keys())[0]
+            value = list(f.values())[0]
+
+            if key in self.get_variables():
+                if callable(value):
+                    df = df[value(df)]
+                else:
+                    # We assume we're doing an equality
+                    df = df[df[key] == value]
+
+        return LogFactor(df)
 
     def get_variables(self):
         """
@@ -105,7 +146,7 @@ class LogFactor:
             merged = left_df.merge(right_df, on='cross-join')
         return merged, variables
 
-    def sum(self, var):
+    def sum(self, variables):
         """
         Marginalize a variable in log space.
 
@@ -113,12 +154,17 @@ class LogFactor:
             x
 
         Parameters:
-            var: string
+            variables: list or str
                 The variable to be summed out.
 
         Returns: Factor
         """
-        other_vars = list(set(self.get_variables()) - {'value', var})
+        if isinstance(variables, str):
+            variables = [variables]
+
+        other_vars = list(
+            (set(self.get_variables()) - {'value'}) - set(variables)
+        )
         if not other_vars:
             return FactorOne()
 
@@ -129,15 +175,12 @@ class LogFactor:
         # Run this recursively until we have one item per group.
         # Could do pairwise
         self.copy_df = self.df.copy()
-        self.copy_df['rank'] = self.copy_df.groupby(other_vars)['value'].rank()
+        self.copy_df['cumcount'] = self\
+            .copy_df.groupby(other_vars)['value'].transform('cumcount') + 1
 
         # store the last rows that are odd-numbered
-        self.copy_df['max_rank'] = \
-            self.copy_df.groupby(other_vars)['rank'].max()
-
-        # evens. __compute_log_sum_exp__ perfectly handles the even case.
-        self.copy_df = \
-            self.copy_df[self.copy_df['max_rank'] % 2 == 0]
+        self.copy_df.loc[:, 'max_cumcount'] = \
+            self.copy_df.groupby(other_vars)['cumcount'].transform(max)
 
         # If a grouping has odd number of rows, and there is more than 1 row,
         # then we'll need to append this to self.copy_df later after
@@ -150,49 +193,73 @@ class LogFactor:
         # greater than 1.
         to_add = \
             self.copy_df[
-                (self.copy_df['max_rank'] % 2 == 1) &
-                (self.copy_df['max_rank'] != 1)
+                (self.copy_df['max_cumcount'] % 2 == 1) &
+                (self.copy_df['max_cumcount'] != 1) &
+                (self.copy_df['cumcount'] == self.copy_df['max_cumcount'])
             ]
 
         # Finally, after we run the __compute_log_sum_exp__ twice, we'll append
         # the rows that only had one row. Those don't need to be processed.
         only_one_row = \
-            self.copy_df[self.copy_df['max_rank'] == 1]
+            self.copy_df[self.copy_df['max_cumcount'] == 1]
 
-        while any(self.copy_df['rank'] > 1):
+        # evens. __compute_log_sum_exp__ perfectly handles the even case.
+        self.copy_df = \
+            self.copy_df[
+                (self.copy_df['max_cumcount'] % 2 == 0) |
+                (
+                    (self.copy_df['max_cumcount'] % 2 == 1) &
+                    (self.copy_df['max_cumcount'] != self.copy_df['cumcount'])
+                )
+            ]
+
+        # do evens
+        while any(self.copy_df['cumcount'] > 1):
             self.__compute_log_sum_exp__(other_vars)
-            # self.copy_df.drop(columns=['value_lag'], inplace=True)
 
-        self.copy_df = pd.concat([
-            self.copy_df,
-            to_add
-        ])
+        # At this point, we only have one value per groupby. Now we add the
+        # last row we removed from the odd-numbered groups
+        self.copy_df = self.copy_df.append(to_add)
+
+        self.copy_df['cumcount'] = \
+            self.copy_df.groupby(other_vars)['value'].transform('cumcount') + 1
+        self.copy_df.loc[:, 'max_cumcount'] = \
+            self.copy_df.groupby(other_vars)['cumcount'].transform(max)
+
+        # those that don't need more processing
+        good_to_go = self.copy_df[self.copy_df['max_cumcount'] == 1]
+
+        # those that might still need more processing
+        self.copy_df = self.copy_df[self.copy_df['max_cumcount'] != 1]
+
+        vars_to_include = list(set(other_vars).union({'value'}))
 
         # Combine odds
-        self.__compute_log_sum_exp__(other_vars)
+        if self.df.shape[0] > 0:
+            self.__compute_log_sum_exp__(other_vars)
 
-        self.copy_df = pd.concat([
-            self.copy_df,
-            only_one_row
+        returnables = pd.concat([
+            good_to_go[vars_to_include],
+            self.copy_df[vars_to_include],
+            only_one_row[vars_to_include]
         ])
 
         return LogFactor(
-            df=self.copy_df.reset_index()[other_vars + ['value']]
+            df=returnables.reset_index()[other_vars + ['value']]
         )
 
     def __compute_log_sum_exp__(self, other_vars):
         shifted = self\
             .copy_df\
-            .reset_index()\
             .groupby(other_vars)[['value']].shift(-1)
-        lagged = self.copy_df.join(
-            shifted.rename(columns=lambda x: x+"_lag")
+
+        lagged = self.copy_df.merge(
+            shifted.rename(columns=lambda x: x+"_lag"),
+            left_index=True,
+            right_index=True
         )
         self.copy_df['value'] = \
             np.logaddexp(lagged['value'], lagged['value_lag'])
         # TODO: this might be a problem when there are an odd number of
         # rows for a group?
         self.copy_df = self.copy_df.dropna()
-        self.copy_df['rank'] = self\
-            .copy_df.groupby(other_vars)['value'].rank()
-        self.copy_df = self.copy_df[self.copy_df['rank'] % 2 == 1]
--- tests/log_factor_test.py
+++ tests/log_factor_test.py
@@ -1,4 +1,3 @@
-import pytest
 import numpy as np
 import pandas as pd
 from ..linx.log_factor import LogFactor
