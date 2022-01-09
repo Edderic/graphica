3b4f30da7dccf6976f790ee481b8e7a88a572a3b
Edderic Ugaddan <eugaddan@panoramaed.com>
2022-01-09 08:56:36 -0500

    [feat] add clean_tmp and get_tmp_path helper methods

--- linx/misc.py
+++ linx/misc.py
@@ -1,6 +1,8 @@
 """
 Miscellaneous functions
 """
+import os
+from pathlib import Path
 
 
 def get_version():
@@ -9,3 +11,41 @@ def get_version():
     """
 
     return "0.0.8"
+
+
+def get_tmp_path():
+    """
+    Get the tmp path where temporary data for tests get stored.
+    """
+    path = Path(f"{os.getenv('HOME')}/tmp_linx_dev")
+    path.mkdir(exist_ok=True)
+
+    return path
+
+
+def clean_tmp(
+    path=get_tmp_path(),
+):
+    """
+    Clean the tmp folder.
+
+    Parameters:
+        path: Path-like object
+    """
+    path.mkdir(exist_ok=True)
+
+    matches = list(path.rglob("*"))
+    for match in matches:
+        if not match.is_dir():
+            match.unlink()
+
+    descending_matches = sorted(
+        matches,
+        key=lambda path: len(path.parents),
+        reverse=True
+    )
+
+    # Delete more nested folders first.
+    for match in descending_matches:
+        if match.is_dir():
+            match.rmdir()
