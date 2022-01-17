5596ad3a805a5214ef76a3c1cdb433054e945b80
Edderic Ugaddan <eugaddan@panoramaed.com>
2022-01-17 17:12:40 -0500

    [feat] refactor Factor into LogFactorAdapter

--- linx/ds.py
+++ linx/ds.py
@@ -4,6 +4,7 @@ Data Structures module.
 Classes:
     - ConditionalProbabilityTable
     - DirectedAcyclicGraph
+    - LogFactorAdapter
     - Factor
     - Factors
     - BayesianNetwork
@@ -13,6 +14,7 @@ from .query import Query
 from .conditional_probability_table import ConditionalProbabilityTable
 from .directed_acyclic_graph import DirectedAcyclicGraph
 from .factor import Factor
+from .log_factor_adapter import LogFactorAdapter
 from .factors import Factors
 from .markov_network import MarkovNetwork
 from .bayesian_network import BayesianNetwork
