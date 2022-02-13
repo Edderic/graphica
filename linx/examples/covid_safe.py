503bb74bda86c2365529ea95d85fde955f0a812e
Edderic Ugaddan <eugaddan@panoramaed.com>
2022-02-13 16:45:21 -0500

    [feat] create_dose_from_strangers
    ... and
    - create_avg_mask_reduction_from_others
    - round_column
    - create_at_least_one_inf

--- linx/examples/covid_safe.py
+++ linx/examples/covid_safe.py
@@ -1068,6 +1068,306 @@ def create_activity_specific_breathing_rate_df(
     return cpt_df
 
 
+def create_avg_mask_reduction_from_others(suffix):
+    avg_mask_reduction_from_others = multiply_by(
+        factor_1_unique=np.arange(0.0, 1.1, 0.1).round(1),
+        factor_1_name=f'perc_masked_{suffix}',
+        factor_2_unique=[
+            0, 0.3, 0.5, 0.8, 0.9, 0.95, 0.99
+        ],
+        factor_2_name=f'mask_quality_{suffix}',
+        new_key='mask_product'
+    )
+
+    new_key = f'avg_mask_reduction_{suffix}'
+    avg_mask_reduction_from_others[new_key] = \
+        1.0 - avg_mask_reduction_from_others['mask_product']
+
+    avg_mask_reduction_from_others.drop(
+        columns=['mask_product'],
+        inplace=True
+    )
+
+    avg_mask_reduction_from_others['value'] = 1.0
+
+    return avg_mask_reduction_from_others, new_key
+
+
+def round_column(df, key, rounding, lower_bound=1):
+    """
+    Round numbers to decrease the memory footprint.
+    """
+    df[key].mask(
+        df[key] > lower_bound,
+        df[key].round(0),
+        inplace=True
+    )
+
+    df.loc[:, key] = df[key].round(rounding)
+
+
+def create_at_least_one_inf(
+    time,
+    prefix,
+    suffix,
+    bayesian_network,
+    storage_folder=None
+):
+    num_pos_cases = [
+        0, 100, 200, 300, 400, 500, 600, 700, 800, 900,
+        1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000,
+        10000, 20000, 30000, 40000, 50000, 60000, 70000, 80000,
+        90000, 100_000, 200_000, 300_000, 400_000, 500_000, 600_000,
+        700_000, 800_000, 900_000, 1_000_000, 2_000_000, 3_000_000,
+        4_000_000, 5_000_000, 6_000_000, 7_000_000, 8_000_000,
+        9_000_000, 10_000_000, 20_000_000, 30_000_000, 40_000_000,
+        50_000_000, 60_000_000, 70_000_000, 80_000_000, 80_000_000,
+        90_000_000, 100_000_000
+    ]
+
+    pop_size = num_pos_cases[1:]
+
+    time_suffix = index_name(time)
+
+    new_key_9 = f'{prefix}_pop_ratio_{time_suffix}'
+    tmp_9_df = divide_by(
+        factor_1_unique=num_pos_cases,
+        factor_1_name=f'num_positive_cases_{time_suffix}',
+        factor_2_unique=pop_size,
+        factor_2_name=f'pop_size_{time_suffix}',
+        new_key=new_key_9
+    )
+
+    new_key_10 = f'{prefix}_multiplier_1_{time_suffix}'
+    tmp_10_df = multiply_by(
+        factor_1_unique=tmp_9_df[new_key_9].unique(),
+        factor_1_name=new_key_9,
+        factor_2_unique=[7, 8, 9, 10],
+        factor_2_name=f'num_days_inf_{time_suffix}',
+        new_key=new_key_10
+    )
+
+    new_key_11 = f'infectious_proba_{time_suffix}'
+    tmp_11_df = multiply_by(
+        factor_1_unique=tmp_10_df[new_key_10].unique(),
+        factor_1_name=new_key_10,
+        factor_2_unique=[5, 6, 7, 8, 9, 10],
+        factor_2_name=f'unreported_positive_{time_suffix}',
+        new_key=new_key_11
+    )
+    round_column(tmp_11_df, new_key_11, rounding=4)
+    tmp_11_df = tmp_11_df[tmp_11_df[new_key_11] < 1]
+
+    parameters = {
+        f'num_people_seen_{suffix}': list(range(0, 51)),
+        f'infectious_proba_{time_suffix}': tmp_11_df[new_key_11].unique()
+    }
+
+    dtypes = {
+        f'num_people_seen_{suffix}': 'float64',
+        f'infectious_proba_{time_suffix}':
+            'float64',
+    }
+
+    new_key_12 = f'at_least_one_inf_{suffix}'
+    tmp_12_df = mega_join_cross_product(parameters, dtypes)
+    tmp_12_df['value'] = 1.0 - (
+        1.0 - tmp_12_df[f'infectious_proba_{time_suffix}']
+    ) ** tmp_12_df[f'num_people_seen_{suffix}']
+
+    # Add the complementary event (at_least_one_inf = 0)
+    tmp_12_df[new_key_12] = 1.0
+    copy = tmp_12_df.copy()
+    copy[new_key_12] = 0
+    copy['value'] = 1.0 - copy['value']
+
+    tmp_12_df = pd.concat([
+        tmp_12_df, copy
+    ])
+
+    keys = [
+        new_key_9,
+        new_key_10,
+        new_key_11,
+        new_key_12,
+    ]
+
+    dfs = [
+        tmp_9_df,
+        tmp_10_df,
+        tmp_11_df,
+        tmp_12_df,
+    ]
+
+    add_dfs_to_bn(
+        bayesian_network,
+        dfs,
+        keys,
+        storage_folder=storage_folder
+    )
+
+    return {
+        'at_least_one_inf_key': new_key_12,
+        'at_least_one_inf_df': tmp_12_df
+    }
+
+
+def create_dose_from_strangers(
+    time,
+    person,
+    event,
+    bayesian_network,
+    storage_folder=None,
+    rounding=5
+):
+    volume_vent_key, volume_vent_event_df = create_room_event(
+        event_suffix=index_name(time, person, event),
+        bayesian_network=bayesian_network
+    )
+
+    suffix = index_name(time, event, "others", person)
+    date_event_self_suffix = index_name(time, event, person)
+
+    res = create_activity_exhalation(
+        suffix,
+        bayesian_network,
+        storage_folder=storage_folder
+    )
+    prefix = 'dose_tmp'
+
+    new_key_1 = f"{prefix}_1_{suffix}"
+    tmp_1_df = multiply_by(
+        factor_1_unique=volume_vent_event_df[volume_vent_key].unique(),
+        factor_1_name=volume_vent_key,
+        factor_2_unique=res['activity_exhalation_df'][
+            res['exhalation_factor_key']
+        ].unique(),
+        factor_2_name=res['exhalation_factor_key'],
+        new_key=new_key_1
+    )
+    round_column(tmp_1_df, new_key_1, rounding=rounding)
+
+    inhalation_rate_key = \
+        f'inhalation_rate_{index_name(time, event, person)}'
+
+    tmp_2_df = create_activity_specific_breathing_rate_df(
+        person,
+        time,
+        event,
+        inhalation_rate_key
+    )
+
+    new_key_3 = f"{prefix}_3_{suffix}"
+    tmp_3_df = multiply_by(
+        factor_1_unique=tmp_2_df[inhalation_rate_key].unique(),
+        factor_1_name=inhalation_rate_key,
+        factor_2_unique=tmp_1_df[new_key_1].unique(),
+        factor_2_name=new_key_1,
+        new_key=new_key_3
+    )
+
+    round_column(tmp_3_df, new_key_3, rounding=4)
+
+    new_key_4 = f'{prefix}_4_{suffix}'
+    tmp_4_df = multiply_by(
+        factor_1_unique=tmp_3_df[new_key_3].unique(),
+        factor_1_name=new_key_3,
+        factor_2_unique=[
+            0.25, 0.5, 1, 2, 3, 5, 8, 13, 21
+        ],
+        factor_2_name=f'duration_{suffix}',
+        new_key=new_key_4
+    )
+    round_column(tmp_4_df, new_key_4, rounding=4)
+
+    new_key_5 = f'{prefix}_5_{suffix}'
+    tmp_5_df = multiply_by(
+        factor_1_unique=tmp_4_df[new_key_4].unique(),
+        factor_1_name=new_key_4,
+        factor_2_unique=[
+            0, 10, 20, 30, 40, 50, 60, 70, 80
+        ],
+        factor_2_name=f'quanta_{suffix}',
+        new_key=new_key_5
+    )
+    round_column(tmp_5_df, new_key_5, rounding=4)
+
+    tmp_6_df, new_key_6 = create_avg_mask_reduction_from_others(suffix)
+
+    new_key_7 = f'{prefix}_7_{suffix}'
+    tmp_7_df = multiply_by(
+        factor_1_unique=tmp_5_df[new_key_5].unique(),
+        factor_1_name=new_key_5,
+        factor_2_unique=tmp_6_df[new_key_6].unique(),
+        factor_2_name=new_key_6,
+        new_key=new_key_7
+    )
+    round_column(tmp_7_df, new_key_7, rounding=4)
+    cap(df=tmp_7_df, key=new_key_7, maximum=1000)
+
+    new_key_8 = f'{prefix}_8_{suffix}'
+    tmp_8_df = multiply_by(
+        factor_1_unique=tmp_7_df[new_key_7].unique(),
+        factor_1_name=new_key_7,
+        factor_2_unique=[1, 0.7, 0.4, 0.10, 0.01],
+        factor_2_name=f'mask_{date_event_self_suffix}',
+        new_key=new_key_8
+    )
+    round_column(tmp_8_df, new_key_8, rounding=4)
+    cap(df=tmp_8_df, key=new_key_8, maximum=1000)
+
+    dictionary = create_at_least_one_inf(
+        time,
+        prefix,
+        suffix,
+        bayesian_network,
+        storage_folder=storage_folder
+    )
+
+    new_key_12 = dictionary['at_least_one_inf_key']
+    tmp_12_df = dictionary['at_least_one_inf_df']
+
+    new_key_13 = f'{prefix}_13_{suffix}'
+    tmp_13_df = multiply_by(
+        factor_1_unique=tmp_12_df[new_key_12].unique(),
+        factor_1_name=new_key_12,
+        factor_2_unique=tmp_8_df[new_key_8].unique(),
+        factor_2_name=new_key_8,
+        new_key=new_key_13
+    )
+
+    keys = [
+        new_key_1,
+        inhalation_rate_key,
+        new_key_3,
+        new_key_4,
+        new_key_5,
+        new_key_6,
+        new_key_7,
+        new_key_8,
+        new_key_13
+    ]
+
+    dfs = [
+        tmp_1_df,
+        tmp_2_df,
+        tmp_3_df,
+        tmp_4_df,
+        tmp_5_df,
+        tmp_6_df,
+        tmp_7_df,
+        tmp_8_df,
+        tmp_13_df
+    ]
+
+    add_dfs_to_bn(
+        bayesian_network,
+        dfs,
+        keys,
+        storage_folder=storage_folder
+    )
+
+
 def create_dose_pair_between(
     potential_infector,
     potential_infectee,
@@ -1220,33 +1520,27 @@ def add_dfs_to_bn(
 
 
 def create_activity_exhalation(
-    time,
-    event,
-    person,
+    suffix,
     bayesian_network,
     storage_folder=None
 ):
-    suffix = index_name(time, event, person)
-    inhalation_rate_key = \
-        f'inhalation_rate_{suffix}'
-
     exhalation_factor_key = \
         f'exhalation_factor_{suffix}'
 
     activity_exhalation_df = pd.DataFrame({
         f'activity_exhalation_{suffix}': [
-            "Resting – Oral breathing",
-            "Resting – Speaking",
-            "Resting – Loudly speaking",
-            "Standing – Oral breathing",
-            "Standing – Speaking",
-            "Standing – Loudly speaking",
-            "Light exercise – Oral breathing",
-            "Light exercise – Speaking",
-            "Light exercise – Loudly speaking",
-            "Heavy exercise – Oral breathing",
-            "Heavy exercise – Speaking",
-            "Heavy exercise – Loudly speaking"
+            "Resting - Oral breathing",
+            "Resting - Speaking",
+            "Resting - Loudly speaking",
+            "Standing - Oral breathing",
+            "Standing - Speaking",
+            "Standing - Loudly speaking",
+            "Light exercise - Oral breathing",
+            "Light exercise - Speaking",
+            "Light exercise - Loudly speaking",
+            "Heavy exercise - Oral breathing",
+            "Heavy exercise - Speaking",
+            "Heavy exercise - Loudly speaking"
         ],
         exhalation_factor_key: [
             1, 4.7, 30.3, 1.2, 5.7, 32.6, 2.8, 13.2, 85, 6.8, 31.6, 204
@@ -1267,8 +1561,8 @@ def create_activity_exhalation(
     )
 
     return {
-        'inhalation_rate_key': inhalation_rate_key,
-        'exhalation_factor_key': exhalation_factor_key
+        'exhalation_factor_key': exhalation_factor_key,
+        'activity_exhalation_df': activity_exhalation_df
     }
 
 
@@ -1284,10 +1578,9 @@ def create_doses(
 ):
 
     for p1 in people:
+        suffix = index_name(time, event, p1)
         activity_exh_dict = create_activity_exhalation(
-            time,
-            event,
-            p1,
+            suffix,
             bayesian_network,
             storage_folder=storage_folder
         )
