25dfbf1b4c8d4d3e977b96c004fa4e6be00335fe
Edderic Ugaddan <eugaddan@panoramaed.com>
2022-03-20 16:25:20 -0400

    [test] factors#filter

--- tests/factors_test.py
+++ tests/factors_test.py
@@ -1,3 +1,5 @@
+import pytest
+
 import pandas as pd
 
 from ..linx.ds import Factors, Factor
@@ -104,3 +106,63 @@ def test_get_variables():
     variables = factors.get_variables()
 
     assert {'X', 'Y', 'A'} == variables
+
+
+@pytest.mark.f
+def test_get_filters():
+    clean_tmp()
+
+    df1 = pd.DataFrame([
+        {
+            'X': 0, 'value': 123,
+        },
+        {
+            'X': 1, 'value': 123,
+        }
+    ])
+
+    df2 = pd.DataFrame([
+        {
+            'Y': 0, 'value': 123,
+        },
+        {
+            'Y': 1, 'value': 123,
+        }
+    ])
+
+    df3 = pd.DataFrame([
+        {
+            'Y': 0, 'A': 0, 'value': 123,
+        },
+        {
+            'Y': 0, 'A': 1, 'value': 123,
+        },
+        {
+            'Y': 1, 'A': 0, 'value': 123,
+        },
+        {
+            'Y': 1, 'A': 1, 'value': 123,
+        },
+    ])
+
+    factor_1 = Factor(
+        ParquetData(df1, storage_folder=get_tmp_path())
+    )
+    factor_2 = Factor(
+        ParquetData(df2, storage_folder=get_tmp_path())
+    )
+    factor_3 = Factor(
+        ParquetData(df3, storage_folder=get_tmp_path())
+    )
+    factors = Factors([factor_1, factor_2, factor_3])
+
+    filters = {
+        'A': 0
+    }
+
+    filtered = factors.filter(filters)
+
+    assert set(filtered[2].get_variables()) == set(['Y', 'A'])
+    assert all(filtered[2].get_df()['A'] == 0)
+
+    clean_tmp()
